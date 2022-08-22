1308c264ccd111dcbabcdc9837a7abf8bedcc293
yingjianting <1244746663@qq.com>
2022-08-22 21:01:27 +0800

    初始化结构创建

---
+++ gulimall-order/src/test/java/com/ying/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ying.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
